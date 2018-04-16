ee8fdd11f79c08ce4cdb5c5283425da748a3e8b6
Kennedy Mota <kennedyanderson98@gmail.com>
2018-04-16 17:38:31 -0300

    # ADICIONADO OS METODOS PARA CHAMAR O DAO

--- projeto_cartorio/src/br/usjt/projeto/service/SenhaService.java
+++ projeto_cartorio/src/br/usjt/projeto/service/SenhaService.java
@@ -66,4 +66,8 @@ public class SenhaService {
 		return fila.getSigla() + siglaServico + numero;
 	}
 
+	public int pegarTotalSenhasServico(int idServico) {
+		return dao.pegarTotalSenhasServico(idServico);
+	}
+
 }

7c15b57a652c32a64de771b4f0433371c94d7330
Kennedy Mota <kennedyanderson98@gmail.com>
2018-04-16 17:19:26 -0300

    # FINALIZADO OS METODOS PARA EXIBIR O TEMPO MÉDIO DE ATENDIMENTO PARA UM SERVIÇO NA DATA ATUAL DO SISTEMA

--- projeto_cartorio/src/br/usjt/projeto/dao/AtendimentoDAO.java
+++ projeto_cartorio/src/br/usjt/projeto/dao/AtendimentoDAO.java
@@ -5,11 +5,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,28 +47,54 @@ public class AtendimentoDAO {
 		return manager.createQuery("select a from Atendimento a where a.status = 'ABERTO' group by a.senha").getResultList();
 	}
 	
-	public double tempoMedioAtendimento(int totalSenha, int tempoTotalAtendimento) {
-		return -1;
+	public long tempoMedioAtendimentoServico(int idServico, int qtdSenhas) {
+		System.out.println("------------------------------------------");
+		System.out.println("Tempo Médio Atendimento");
+		System.out.println("Quantidade Senhas: " + qtdSenhas);
+		System.out.println("Tempo médio: " + pegarTempoTotalAtendimentoServico(idServico) / qtdSenhas);
+		long tempoMedio = pegarTempoTotalAtendimentoServico(idServico) / qtdSenhas;
+		System.out.println("Tempo médio de Atendimento para o Serviço de id: " + idServico);
+		System.out.println("Horas: " + (tempoMedio / (1000 * 60 * 60)) + " Minutos: " + ((tempoMedio / 60000) % 60) + " Segundos: " + ((tempoMedio / 1000) % 60) );
+		System.out.println("------------------------------------------");
+		return pegarTempoTotalAtendimentoServico(idServico) / qtdSenhas;
+		
 	}
 	
-	public double pegarTempoTotalAtendimentoServico(int idServico) {
-		double tempoTotal = 0;
-		double tempoInicio;
-		double tempoFim;
+	public long pegarTempoTotalAtendimentoServico(int idServico) {
+		long tempoTotal = 0;
+		long tempoInicio;
+		long tempoFim;
 		long horas;
 		long minutos;
+		Date data = new Date();
+		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
+		
 		String sqlSelect = "SELECT atd.id, atd.id_senha, atd.id_subservico, atd.data_inicio, atd.data_termino, atd.status, sub_serv.id_servico FROM atendimento atd"
-				+ "JOIN subservico sub_serv ON atd.id_subservico = sub_serv.id"
-				+ "WHERE sub_serv.id_servico = :id_servico";
+				+ " JOIN subservico sub_serv ON atd.id_subservico = sub_serv.id"
+				+ " WHERE sub_serv.id_servico = ? AND atd.data_inicio LIKE ? ?";
 		
 		try (PreparedStatement pst = conn.prepareStatement(sqlSelect);){
 			pst.setInt(1, idServico);
+			pst.setString(2, fmt.format(data));
+			pst.setString(3, "%");
 			try(ResultSet rs = pst.executeQuery();){
 				while(rs.next()) {
-					long horasInicio = (rs.getDate("data_inicio").getTime());
-					long horasFim = (rs.getDate("data_termino").getTime());
-					tempoTotal = tempoTotal + ((horasFim - horasInicio) / 3600000);
+					System.out.println("------------------------------------------");
+					System.out.println("Pegar Tempo Total Atendimento Servico");
+					long horasInicio = (rs.getTime("data_inicio").getTime());
+					System.out.println("hora inicio: " + rs.getTime("data_inicio"));
+					System.out.println("hora inicio: " + horasInicio);
+					long horasFim = (rs.getTime("data_termino").getTime());
+					System.out.println("hora fim: " + rs.getTime("data_termino"));
+					System.out.println("hora fim: " + horasFim);
+					long diff = horasFim - horasInicio;
+					System.out.println("diff: " + diff);
+					System.out.println("Total: " + diff + " Horas: " + (diff / (1000 * 60 * 60)) + "Minutos: " + ((diff / 60000) % 60) + "Segundos: " + ((diff / 1000) % 60));
+					tempoTotal = tempoTotal + diff;
 				}
+				System.out.println("Tempo Total Completo: " + tempoTotal + " Horas: " + (tempoTotal / (1000 * 60 * 60)) +
+						" Minutos: " + ((tempoTotal / 60000) % 60) + " Segundos: " + ((tempoTotal / 1000) % 60));
+				System.out.println("------------------------------------------");
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
